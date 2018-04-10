cf9eb3c1e863f257dbab1519b2e4fa71466c649a
Michael Aherne <michael.aherne@strath.ac.uk>
2018-04-10 08:42:55 +0100

    Initial commit

---
+++ tests/xmldb_generator_test.php
@@ -0,0 +1,42 @@
+<?php
+
+use core\persistent;
+use tool_lazydev\local\xmldb_generator;
+
+class tool_lazydev_xmldb_generator_testcase extends \advanced_testcase {
+
+    public function test_tabledef_for_persistent() {
+        $persistent1 = new class extends persistent {
+
+            const TABLE = 'interesting_facts';
+
+            // From docs.
+            protected static function define_properties() {
+                return array(
+                    'userid' => array(
+                        'type' => PARAM_INT,
+                    ),
+                    'message' => array(
+                        'type' => PARAM_RAW,
+                    )
+                );
+            }
+        };
+
+        $gen = new xmldb_generator();
+        $def = $gen->tabledef_for_persistent($persistent1);
+        echo $def->xmlOutput();
+        $fields = $def->getFields();
+        $this->assertEquals('id', $fields[0]->getName(), 'id field should be first');
+        $this->assertNotEmpty($def->getField('timecreated'));
+        $this->assertEquals(XMLDB_TYPE_INTEGER, $def->getField('userid')->getType());
+
+    }
+
+    public function test_infer_column_details() {
+        $gen = new xmldb_generator();
+        $paramint = $gen->infer_column_details(PARAM_INT);
+        $this->assertEquals(['type' => XMLDB_TYPE_INTEGER, 'precision' => 10], $paramint);
+    }
+
+}
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2018040600; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2017111300; // Requires this Moodle version
+$plugin->component = 'tool_lazydev'; // Full name of the plugin (used for diagnostics)
+
